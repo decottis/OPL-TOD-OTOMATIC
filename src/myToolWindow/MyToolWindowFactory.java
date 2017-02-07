c9359ab65cd647ce520f3cc06822c5fd1d545845
decottis <decottignies.s@gmail.com>
2017-02-07 15:16:55 +0100

    [fix] fitou help

--- src/myToolWindow/MyToolWindowFactory.java
+++ src/myToolWindow/MyToolWindowFactory.java
@@ -2,9 +2,15 @@ package myToolWindow;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.*;
+import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.content.*;
+import com.intellij.ui.treeStructure.Tree;
 
 import javax.swing.*;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 import java.awt.event.*;
 import java.util.Calendar;
 
@@ -14,7 +20,7 @@ import java.util.Calendar;
  * Date: Aug 25, 2010
  * Time: 2:09:00 PM
  */
-public class MyToolWindowFactory implements ToolWindowFactory {
+public class MyToolWindowFactory implements ToolWindowFactory, TreeSelectionListener {
 
     private JButton refreshToolWindowButton;
     private JButton hideToolWindowButton;
@@ -22,55 +28,58 @@ public class MyToolWindowFactory implements ToolWindowFactory {
     private JLabel currentTime;
     private JLabel timeZone;
     private JPanel myToolWindowContent;
+    private JTree tree1;
     private ToolWindow myToolWindow;
 
 
     public MyToolWindowFactory() {
-        hideToolWindowButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                myToolWindow.hide(null);
-            }
-        });
-        refreshToolWindowButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                MyToolWindowFactory.this.currentDateTime();
-            }
-        });
+
     }
 
     // Create the tool window content.
     public void createToolWindowContent(Project project, ToolWindow toolWindow) {
         myToolWindow = toolWindow;
-        this.currentDateTime();
+
+        //make nodes - test nodes
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode("tree1");
+
+        DefaultMutableTreeNode lv1 = new DefaultMutableTreeNode("lv1");
+        DefaultMutableTreeNode lv12 = new DefaultMutableTreeNode("lv12");
+        DefaultMutableTreeNode lv2 = new DefaultMutableTreeNode("lv2");
+
+
+        root.add(lv1);
+        lv1.add(lv12);
+        root.add(lv2);
+
+        lv2.add( new DefaultMutableTreeNode("lv21"));
+        lv2.add( new DefaultMutableTreeNode("lv22"));
+        lv2.add( new DefaultMutableTreeNode("lv23"));
+
+        //make tree
+        JTree filesInConnection = new Tree(root);
+        filesInConnection.addTreeSelectionListener(this);
+
+
+        DefaultTreeModel treeModel = new DefaultTreeModel(root);
+        filesInConnection.setModel(treeModel);
+        filesInConnection.setRootVisible(true);
+
+        JScrollPane filesScroll = new JBScrollPane(filesInConnection);
+
+        //get content factory instance
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+
+        //create content panel
         Content content = contentFactory.createContent(myToolWindowContent, "", false);
-        toolWindow.getContentManager().addContent(content);
+        Content f = contentFactory.createContent(filesInConnection, "", false);
+
+        toolWindow.getContentManager().addContent(content, 0);
+        toolWindow.getContentManager().addContent(f, 0);
 
     }
 
-    public void currentDateTime() {
-        // Get current date and time
-        Calendar instance = Calendar.getInstance();
-        currentDate.setText(String.valueOf(instance.get(Calendar.DAY_OF_MONTH)) + "/"
-                + String.valueOf(instance.get(Calendar.MONTH) + 1) + "/" +
-                String.valueOf(instance.get(Calendar.YEAR)));
-        currentDate.setIcon(new ImageIcon(getClass().getResource("/myToolWindow/Calendar-icon.png")));
-        int min = instance.get(Calendar.MINUTE);
-        String strMin;
-        if (min < 10) {
-            strMin = "0" + String.valueOf(min);
-        } else {
-            strMin = String.valueOf(min);
-        }
-        currentTime.setText(instance.get(Calendar.HOUR_OF_DAY) + ":" + strMin);
-        currentTime.setIcon(new ImageIcon(getClass().getResource("/myToolWindow/Time-icon.png")));
-        // Get time zone
-        long gmt_Offset = instance.get(Calendar.ZONE_OFFSET); // offset from GMT in milliseconds
-        String str_gmt_Offset = String.valueOf(gmt_Offset / 3600000);
-        str_gmt_Offset = (gmt_Offset > 0) ? "GMT + " + str_gmt_Offset : "GMT - " + str_gmt_Offset;
-        timeZone.setText(str_gmt_Offset);
-        timeZone.setIcon(new ImageIcon(getClass().getResource("/myToolWindow/Time-zone-icon.png")));
-
+    public void valueChanged(TreeSelectionEvent arg0) {
 
     }
 
